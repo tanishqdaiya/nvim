d2e080f346ae46ea587a41dfd904e127fdfd6985
Tanishq Daiya <tanishqdaiya@yahoo.com>
2023-12-28 23:59:48 +0530

    fix: Based on the previous readme, the useless extensions have been removed and the useful keybindings have been added, such as the copying of text

--- lua/tanishqdaiya/core/keymap.lua
+++ lua/tanishqdaiya/core/keymap.lua
@@ -5,3 +5,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
